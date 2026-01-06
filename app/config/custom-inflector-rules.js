01876e22ac6e232ef0ce9a60160fa00dc14d51f4
Achraf <achrafatauil@hotmail.com>
2026-01-06 23:32:30 +0100

    add ember-data adapters, serializers, utils & inflector files

---
+++ app/config/custom-inflector-rules.js
@@ -0,0 +1,5 @@
+import { irregular } from '@ember-data/request-utils/string';
+
+irregular('bestuurseenheid', 'bestuurseenheden');
+irregular('period-of-time', 'periods-of-time');
+irregular('account', 'accounts');
---
+++ app/serializers/application.js
@@ -0,0 +1,12 @@
+/* eslint-disable ember/no-mixins */
+import JSONAPISerializer from '@ember-data/serializer/json-api';
+import DataTableSerializerMixin from 'frontend-ipdc-enrichment/mixins/ember-data-table/serializer';
+
+export default class ApplicationSerializer extends JSONAPISerializer.extend(
+  DataTableSerializerMixin,
+) {
+  serializeAttribute(snapshot, json, key, attributes) {
+    if (key !== 'uri')
+      super.serializeAttribute(snapshot, json, key, attributes);
+  }
+}
