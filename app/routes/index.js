8b4c913b6fa2b3fb2116d3fb6cd64dec3f9f7bad
Achraf <achrafatauil@hotmail.com>
2026-01-20 15:08:31 +0100

    add basic contactpunt form instance

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,17 @@ import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service session;
+  @service semanticFormRepository;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+  }
+
+  async model() {
+    const form = await this.semanticFormRepository.getFormDefinition(
+      "contactpunt"
+    );
+    console.log(form);
+    return form
   }
 }
