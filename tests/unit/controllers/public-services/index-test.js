2e6177e2ac18f092d8317f048655a6c133c1bd45
Achraf <achrafatauil@hotmail.com>
2026-02-04 09:45:19 +0100

    commit test files

--- tests/unit/controllers/public-services/index-test.js
+++ tests/unit/controllers/public-services/index-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'frontend-ipdc-enrichment/tests/helpers';
 
-module('Unit | Controller | public-service/index', function (hooks) {
+module('Unit | Controller | public-services/index', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
-    let controller = this.owner.lookup('controller:public-service/index');
+    let controller = this.owner.lookup('controller:public-services/index');
     assert.ok(controller);
   });
 });
