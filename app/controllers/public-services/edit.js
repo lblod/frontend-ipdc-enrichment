c1898833e01cbccfb8ff7edf7dbc9712fe1a9696
Achraf <achrafatauil@hotmail.com>
2026-02-05 16:20:53 +0100

    cleanup console.log statement

--- app/controllers/public-services/edit.js
+++ app/controllers/public-services/edit.js
@@ -3,7 +3,6 @@ import Controller from '@ember/controller';
 export default class PublicServicesEditController extends Controller {
 
   get badgeSkin() {
-    console.log(this.model)
     if (this.model.publicService.relevantAdministrativeUnits) {
       return 'success';
     } else {
