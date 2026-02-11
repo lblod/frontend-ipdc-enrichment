ede4e0205d83ecd88c9127c4228848e7790c785c
Achraf <achrafatauil@hotmail.com>
2026-02-11 17:04:11 +0100

    fix annoying test

--- tests/integration/components/broader-concept-pills-test.js
+++ tests/integration/components/broader-concept-pills-test.js
@@ -2,25 +2,24 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'frontend-ipdc-enrichment/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
 
 module('Integration | Component | broader-concept-pills', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders label for provided uri', async function (assert) {
+    class StoreStub extends Service {
+      findRecordByUri() {
+        return Promise.resolve({ label: 'Example concept' });
+      }
+    }
 
-    await render(hbs`<BroaderConceptPills />`);
+    this.owner.register('service:store', StoreStub);
 
-    assert.dom().hasText('');
+    this.set('uri', 'http://example.com/concept/1');
 
-    // Template block usage:
-    await render(hbs`
-      <BroaderConceptPills>
-        template block text
-      </BroaderConceptPills>
-    `);
+    await render(hbs`<BroaderConceptPills @uri={{this.uri}} />`);
 
-    assert.dom().hasText('template block text');
+    assert.dom().hasText('Example concept');
   });
 });
