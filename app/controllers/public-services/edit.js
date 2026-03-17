559923b7127ad34a316b76287199b4abc832fc59
wolfez <wolfderechter@gmail.com>
2026-03-17 10:28:42 +0100

    simplify states

--- app/controllers/public-services/edit.js
+++ app/controllers/public-services/edit.js
@@ -1,26 +1,28 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 
 export default class PublicServicesEditController extends Controller {
   @service store;
-  @tracked relevantAdministrativeUnits = this.model.publicService?.relevantAdministrativeUnits ?? [];
+
+  get hasAdminUnits() {
+    return (
+      (this.model.publicService.relevantAdministrativeUnits?.length ?? 0) > 0
+    );
+  }
 
   get badgeSkin() {
-    return this.relevantAdministrativeUnits?.length ? 'success' : 'grey';
+    return this.hasAdminUnits ? 'success' : 'grey';
   }
+
   get icon() {
-    return this.relevantAdministrativeUnits?.length ? 'check' : '';
+    return this.hasAdminUnits ? 'check' : '';
   }
 
   @action
   async onSave() {
-    const refreshed = await this.store.findRecord(
-      'public-service',
-      this.model.publicService.id,
-      { reload: true, include: 'relevant-administrative-units' }
-    );
-    this.model.publicService = refreshed;
+    await this.model.publicService.reload({
+      include: 'relevant-administrative-units',
+    });
   }
 }
