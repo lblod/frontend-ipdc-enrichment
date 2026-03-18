156e06fb9abca5cbbe2ba1842a62fa701bc42544
Achraf <achrafatauil@hotmail.com>
2026-03-18 09:33:29 +0100

    replace broken date format helper by date util (ref: frontend-ipdc)

--- app/helpers/format-date.js
+++
@@ -1,4 +0,0 @@
-// TODO: double check if this export was even public in Appuniversum, I believe it was an internal thing that got exposed by Ember's old global requirements.
-import { formatDate } from 'frontend-ipdc-enrichment/utils/date';
-
-export default formatDate;
