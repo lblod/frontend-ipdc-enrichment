4f21ab849299498507c79e4afe52c3d1785d7a4b
wolfez <wolfderechter@gmail.com>
2026-04-01 10:16:07 +0200

    remove custom disabled textarea in favor of form:fieldDisabled

--- app/components/rdf-form-fields/disabled-text-area.js
+++
@@ -1,3 +0,0 @@
-import RdfInputFieldsTextAreaComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/text-area';
-
-export default class RdfFormFieldsDisabledTextArea extends RdfInputFieldsTextAreaComponent {}
--- app/routes/public-services/edit.js
+++ app/routes/public-services/edit.js
@@ -1,17 +1,9 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import { registerFormFields } from '@lblod/ember-submission-form-fields';
-import RdfFormFieldsDisabledTextArea from 'frontend-ipdc-enrichment/components/rdf-form-fields/disabled-text-area';
 
 export default class PublicServicesEditRoute extends Route {
   @service store;
   @service semanticFormRepository;
-
-  constructor() {
-    super(...arguments);
-
-    this.registerTableFields();
-  }
 
   async model(params) {
     const publicService = await this.store.findRecord(
@@ -22,15 +14,5 @@ export default class PublicServicesEditRoute extends Route {
     const form =
       await this.semanticFormRepository.getFormDefinition('public-service');
     return { publicService, form };
-  }
-
-  registerTableFields() {
-    registerFormFields([
-      {
-        displayType: 'http://lblod.data.gift/display-types/disabledTextArea',
-        edit: RdfFormFieldsDisabledTextArea,
-        show: RdfFormFieldsDisabledTextArea,
-      },
-    ]);
   }
 }
